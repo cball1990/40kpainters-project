2ba6051fc7ddb5baa6a53ab3bdd7eb00e19820e1
cball1990 <craig_5050@msn.com>
2020-08-04 19:30:07 +0100

    altered static file settings due to heroku deployment failure

--- minipainters/settings.py
+++ minipainters/settings.py
@@ -151,6 +151,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL ='/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
