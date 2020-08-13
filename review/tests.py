3996b254a023ca5b9edbbe99744da147c58f3619
cball1990 <craig_5050@msn.com>
2020-08-13 02:37:22 +0100

    wrote a test for the review model

--- review/tests.py
+++ review/tests.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 # models test
-class ReviewTest(TestCase):
+class add_revTest(TestCase):
     
-    def create_review(self, name="dave", body="test",  score="3"):
-        return Review.objects.create(name=name, body=body, pub_date=timezone.now(), score=score)
+    def create_add_rev(self, name="dave", body="test",  score="3"):
+        return add_rev.objects.create(name=name, body=body, pub_date=timezone.now(), score=score)
 
-    def test_review_creation(self):
-        w = self.create_review()
-        self.assertTrue(isinstance(w, Review))
+    def test_add_rev_creation(self):
+        w = self.create_add_rev()
+        self.assertTrue(isinstance(w, add_rev))
         self.assertEqual(w.__unicode__(), w.name)
