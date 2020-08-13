e706387ef8818edbb2edf74d06551aee75e0c07d
cball1990 <craig_5050@msn.com>
2020-08-13 02:40:32 +0100

    wrote a test for the review model

--- review/tests.py
+++ review/tests.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from review.models import add_rev
 from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import User
 
 # models test
 class add_revTest(TestCase):
     
-    def create_add_rev(self, name="dave", body="test",  score="3"):
-        return add_rev.objects.create(name=name, body=body, pub_date=timezone.now(), score=score)
+    def create_add_rev(self, body="test",  score="3"):
+        return add_rev.objects.create(name=request.user, body=body, pub_date=timezone.now(), score=score)
 
     def test_add_rev_creation(self):
         w = self.create_add_rev()
