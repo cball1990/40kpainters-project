66a3976f83ca358fb4324bc6f2771e469d6bada2
cball1990 <craig_5050@msn.com>
2020-08-13 01:42:51 +0100

    fixed indentation error in settings

--- minipainters/settings.py
+++ minipainters/settings.py
@@ -104,7 +104,7 @@ if "DATABASE_URL" in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-     print("Database URL not found. Using SQLite instead")
+    print("Database URL not found. Using SQLite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
