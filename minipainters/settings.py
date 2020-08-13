027a69fcecb0d6a6e7383bcb6133a7a7dd71fbb7
cball1990 <craig_5050@msn.com>
2020-08-13 01:56:09 +0100

    corrected database settings

--- minipainters/settings.py
+++ minipainters/settings.py
@@ -99,14 +99,18 @@ WSGI_APPLICATION = 'minipainters.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-
-print("Database URL not found. Using SQLite instead")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
