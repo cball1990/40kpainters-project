488b9cca5ef0eb80f106d11d214572ab3cd58d1a
cball1990 <craig_5050@msn.com>
2020-08-13 02:30:20 +0100

    wrote a test for the review model

--- review/tests.py
+++ review/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from review.models import add_rev
+from django.utils import timezone
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+# models test
+class ReviewTest(TestCase):
+    
+    def create_review(self, name="dave", body="test",  score="3"):
+        return Review.objects.create(name=name, body=body, pub_date=timezone.now(), score=score)
+
+    def test_review_creation(self):
+        w = self.create_Review()
+        self.assertTrue(isinstance(w, Review))
+        self.assertEqual(w.__unicode__(), w.name)
