22e76ac4b5c752344f2465099000d338132a9991
cball1990 <craig_5050@msn.com>
2020-08-13 02:02:54 +0100

    removed unused files

--- products/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from . models import Products
-
-class ProductTests(TestCase):
-    def test_str(self):
-        test_name = Products(name='A Product')
-        self.assertEqal(str(test_name,'A Product'))
-
